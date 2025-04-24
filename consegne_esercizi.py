c57e3ee1a2103073d4ae71f33e637f5e16e3ed0a
MarcoPirrotta0 <104686262+MarcoPirrotta0@users.noreply.github.com>
2025-04-24 10:07:20 +0200

    First Commit

---
+++ consegne_esercizi.py
@@ -0,0 +1,30 @@
+# 1) Scrivere un'espressione che calcola il valore del prodotto tra due numeri interi.
+# 2) Scrivere un'espressione che calcola il valore dell'operazione di esponenziazione tra due numeri interi a e b.
+# 3) Scrivere un'espressione che concatena due stringhe s1 e s2.
+# 4) Scrivere un'espressione che verifica se un numero intero x è pari o dispari.
+# 5) Scrivere un'espressione che calcola il valore dell'area di un cerchio di raggio r.
+# 6) Scrivere un'espressione che calcola la somma dei quadrati di due numeri interi a e b.
+# 7) Scrivere un'espressione che calcola il valore della media aritmetica di una lista di numeri n.
+# 8) Scrivere un'espressione che restituisce il valore massimo di una lista di numeri.
+# 9) Scrivere un identificatore valido e uno non valido.
+# 10) Qual è la convenzione di naming per le variabili e per le costanti in Python?
+# 11) Qual è l'identificatore riservato in Python per indicare una funzione lambda?
+# 12) Scrivere un programma che richiede all'utente d’inserire un numero intero e stampa il suo quadrato. In caso d’inserimento di un valore non valido, il programma deve gestire l'eccezione e stampare un messaggio di errore.
+# 13) Scrivere un programma che apre un file di testo e stampa il suo contenuto riga per riga. In caso di errore nell'apertura del file, il programma deve gestire l'eccezione e stampare un messaggio di errore.
+# 14) Scrivere una funzione che divide due numeri e gestisce l'eccezione di divisione per zero.
+# 15) Scrivere un programma che richiede all'utente d’inserire una lista di numeri interi e ne stampa il valore massimo. In caso d’inserimento di un valore non valido, il programma deve gestire l'eccezione e stampare un messaggio di errore.
+# 16) Scrivere una funzione che calcoli il reciproco di un numero e che gestisca l'eccezione di divisione per zero usando ZeroDivisionError.
+# 17) Scrivere un programma che crea un nuovo file di testo e che scriva una serie di righe in esso.
+# 18) Scrivere un programma che apre un file di testo esistente, legga il suo contenuto e ne stampi ogni riga.
+# 19) Scrivere un programma che copia il contenuto di un file di testo in un altro file.
+# 20) Scrivere un programma che chiede all'utente d’inserire la propria data di nascita (nel formato GG‑MM‑ANNO) e calcoli l'età in giorni, mesi e anni. Il programma deve anche calcolare il giorno della settimana in cui l'utente è nato.
+# 21) Qual è la funzione utilizzata per aprire un file in Python?
+# 22) Qual è il metodo utilizzato per creare una nuova directory in Python?
+# 23) Come si legge un file di testo riga per riga in Python?
+# 24) Qual è il metodo utilizzato per ottenere il percorso assoluto di un file?
+# 25) Qual è il metodo utilizzato per scrivere su un file di testo in Python?
+# 26) Come si calcola la differenza tra due date utilizzando il modulo datetime?
+# 27) Qual è il metodo utilizzato per ottenere il timestamp corrente in Python?
+# 28) Scrivere un listato che richieda all'utente d’inserire un numero intero, e di stampare su schermo "Il numero inserito è pari" se il numero è pari, "Il numero inserito è dispari" se il numero è dispari.
+# 29) Scrivere un programma che chieda all'utente d’inserire il prezzo di un prodotto e il tasso di sconto da applicare. Calcolarne poi il prezzo scontato e stamparlo su schermo.
+# 30) Scrivere un programma che richieda all'utente di inserire un numero intero e di verificare se è pari o dispari. Stampare un messaggio appropriato, a seconda del caso.
